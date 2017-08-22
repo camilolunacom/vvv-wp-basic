9a53dc69a4249d067a99fbdbf4636ff1f147c702
Camilo Luna <camilo@alunizarstudio.com>
2017-08-22 09:52:41 +0100

    Remove xip.io option

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -25,11 +25,6 @@ if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
 
   echo "Configuring WordPress Stable..."
   noroot wp core config --dbname=wordpress_default --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-// Match any requests made via xip.io.
-if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(local.wordpress.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
-    define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
-    define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
-}
 
 define( 'WP_DEBUG', true );
 PHP
